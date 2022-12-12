6c6aabf356b4cb3fd9f000f56b3e337300a91134
Atul yadav <atulyuvasoft411@gmail.com>
2022-12-12 16:41:59 +0530

    second commit

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,7 +3,7 @@ class Employee < ApplicationRecord
 	validates :first_name, :last_name, presence: true
 	validates :personal_email, presence: true, uniqueness: true
 	validates :city, :state, :country, :pincode, :address_line_1, presence: true
-	validates :mobile_number , presence: true
+	validates :mobile_number ,:date_of_birth,:job_title , presence: true
    def name 
    	"#{first_name} #{last_name}".strip
    end	
