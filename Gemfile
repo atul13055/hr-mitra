d624e24f2928ce782563c7562d0b8fe5486b80d2
Atul yadav <atulyuvasoft411@gmail.com>
2022-12-13 13:09:04 +0530

    dropdown

--- Gemfile
+++ Gemfile
@@ -70,3 +70,4 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+gem 'country_select', '~> 6.0'
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ApplicationRecord
   DOC_TYPES = ['Personal ID','Marksheet','Photo ID','Address Proof','Others'].freeze
-
+  
   belongs_to :employee
   validates :name, :doc_type, presence: true
   has_one_attached :image
